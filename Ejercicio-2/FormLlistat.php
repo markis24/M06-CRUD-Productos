635cec5cd36dd511110fb7d52ffe5a8685bab12a
Marc <2023_marc.cuzcano@iticbcn.cat>
2024-03-30 13:14:53 +0100

    Ejercicio 2

--- Ejercicio-2/FormLlistat.php
+++ Ejercicio-2/FormLlistat.php
@@ -5,7 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulari</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+          integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
 
 <?php
@@ -25,8 +26,8 @@ $result = $conn->query($sql);
 $array = array();
 
 if ($result->num_rows > 0) {
-    while($row = $result->fetch_assoc()) {
-        array_push($array, array("id" =>$row["id"], "nom"=>$row["nom"]));            
+    while ($row = $result->fetch_assoc()) {
+        array_push($array, array("id" => $row["id"], "nom" => $row["nom"]));
     }
 } else {
     echo "0 results";
@@ -35,72 +36,84 @@ if ($result->num_rows > 0) {
 $conn->close();
 ?>
 <body class="container mt-5 w-80">
-    <div class="row">
-        <div class="col">
-            <h2 class="mb-3">Formulari</h2>
+<div class="row">
+    <div class="col">
+        <h2 class="mb-3">Formulari</h2>
 
-            <form action="AddEdit.php" method="POST">
-                <div class="form-group mb-2">
-                    <input type="text" class="form-control" id="nomProducte" name="nomProducte" placeholder="Nom" value="">
-                </div>
-                
-                <input type="hidden" name="addEdit" id="addEdit" value="0"/>
-                <button type="submit" class="btn btn-primary">Submit</button>
-            </form> 
-        </div>
-        <div class="col">
-            <h2 class="mb-3">Llistat</h2>
+        <form action="AddEdit.php" method="POST">
+            <div class="form-group mb-2">
+                <input type="text" class="form-control" id="nomProducte" name="nomProducte" placeholder="Nom" value="">
+            </div>
 
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Nom</th>
-                        <th scope="col">Editar</th>
-                        <th scope="col">Esborrar</th>
-                    </tr>
-                </thead>
-                
-                <tbody>
-                    <?php
-                        for($i=0; $i<sizeof($array); $i++){
-                            echo '<tr>
+            <input type="hidden" name="addEdit" id="addEdit" value="0"/>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+    <div class="col">
+        <h2 class="mb-3">Llistat</h2>
+
+        <table class="table">
+            <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Nom</th>
+                <th scope="col">Editar</th>
+                <th scope="col">Esborrar</th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php
+            for ($i = 0; $i < sizeof($array); $i++) {
+                echo '<tr>
                                         <th scope="row">' . $array[$i]["id"] . '</th>
                                         <td>' . $array[$i]["nom"] . '</td>
-                                        <td><p idProd="' . $array[$i]["id"] . '" class="btnEdit btn btn-outline-info">Edit</p></td>
-                                        <td><a href="" class="btn btn-outline-danger">Remove</a></td>
+                                        <td><p idProd="' . $array[$i]["id"] . '" class="btnEdit btn btn-outline-info">Editar</p></td>
+                                        <td><p idProd="' . $array[$i]["id"] . '" class="btnDelete btn btn-outline-danger">Esborrar</p></td>
                                     </tr>';
-                        }  
-                    ?>
-                </tbody>
-            </table>
-        </div>
+            }
+            ?>
+            </tbody>
+        </table>
     </div>
+</div>
 
-    <script>
-        let btnEdit = document.querySelectorAll(".btnEdit");
-        btnEdit.forEach(el=>{
-            el.addEventListener("click", function(){
+<script>
+    let btnEdit = document.querySelectorAll(".btnEdit");
+    btnEdit.forEach(el => {
+        el.addEventListener("click", function () {
 
-                let formData = new FormData();
-                formData.append("id", this.getAttribute("idProd"));
+            let formData = new FormData();
+            formData.append("id", this.getAttribute("idProd"));
 
-                let options = {
-                        method: 'POST',
-                        body: formData
-                    }
+            let options = {
+                method: 'POST',
+                body: formData
+            }
 
-                fetch("getProducte.php", options)
+            fetch("getProducte.php", options)
                 .then((response) => response.json())
                 .then((data) => {
                     console.log(data);
                     document.getElementById("nomProducte").value = data.nom;
                     document.getElementById("addEdit").value = data.addEdit;
                 })
-                .catch((error) => {});
+                .catch((error) => {
+                });
 
-            })
         })
-    </script>
+    })
+    let btnDelete =document.querySelectorAll(".btnDelete");
+    btnDelete.forEach(el =>{
+        el.addEventListener("click", function () {
+            let formData = new FormData();
+            formData.append("id", this.getAttribute("idProd"));
+
+            let options = {
+                method: 'POST',
+                body: formData
+            }
+        }
+        })
+</script>
 </body>
 </html>
--- Ejercicio-2/deleteProducte.php
+++ Ejercicio-2/deleteProducte.php
@@ -1,3 +1,34 @@
 <?php
 
+if(isset($_POST["id"]) && !empty($_POST["id"])){
+    include "DBconfig.php";
+
+// Create connection
+    $conn = mysqli_connect(DB_HOST, DB_USER, DB_PSW, DB_NAME, DB_PORT);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "SELECT * FROM products WHERE id=" . $_POST["id"];
+
+    $result = $conn->query($sql);
+
+    $array = array();
+
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        $object = new stdClass();
+        $object->nom = $row["nom"];
+        $object->addEdit = $row["id"];
+
+        echo json_encode($object);
+
+    } else {
+        echo "0 results";
+    }
+
+    $conn->close();
+}
+
+
 ?>
